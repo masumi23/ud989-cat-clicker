825f4ae9cd9fca55eb7841766c2a016f4d2679a9
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-01-01 14:54:15 -0800

    cat clicker works

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('hi');
+var catImageElem = document.getElementById('cat1');
+var countElem = document.getElementById('count');
+var count = 0;
+
+function plusOne() {
+	count++;
+	countElem.textContent = count;
+}
+
+catImageElem.addEventListener('click', plusOne, false);
