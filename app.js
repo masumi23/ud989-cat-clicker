eb7d85740687e10524dc10c3bdadf9c261df401a
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-01-01 15:23:31 -0800

    cat clicker duo

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var catImageElem = document.getElementById('cat1');
-var countElem = document.getElementById('count');
-var count = 0;
+var catImageElem1 = document.getElementById('cat1');
+var catImageElem2 = document.getElementById('cat2');
+var countElem1 = document.getElementById('count1');
+var countElem2 = document.getElementById('count2');
+var count1 = 0;
+var count2 = 0;
 
-function plusOne() {
-	count++;
-	countElem.textContent = count;
+function plusOne1() {
+	count1++;
+	countElem1.textContent = count1;
 }
 
-catImageElem.addEventListener('click', plusOne, false);
+function plusOne2() {
+	count2++;
+	countElem2.textContent = count2;
+}
+
+catImageElem1.addEventListener('click', plusOne1, false);
+catImageElem2.addEventListener('click', plusOne2, false);
