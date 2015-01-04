f423baf3d9c4d78c390683f6d2b95258430f403e
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-01-03 22:59:07 -0800

    cat clicker premium works now with mvo organization

--- app.js
+++ app.js
@@ -31,34 +31,84 @@ var model = {
 			link: 'cat1.jpg',
 			count: 0
 		}
-	]
+	],
+
+	currentCat: null
 };
 
 var octopus = {
+	
 	// Grab the list of cats from the model so that the view can set it up
 	getCats: function() {
 		return model.catList;
-	}
-	// Determine what the Current Cat because of an event listener is in the view and grab its info from the model so that the view can render it.
+	},
+	
+	// Grab cats info from the model so that the view can render it.
+	setCurrentCat: function(catNum) {
+		model.currentCat = model.catList[catNum];
+		view.renderCat();
+	},
+
+	getCurrentCat: function() {
+		return model.currentCat;
+	},
+	
 	// Find out from the view when a cat picture has been clicked and then update the click count on the model
+	plusOne: function() {
+		model.currentCat.count++;
+		console.log('h00', model.currentCat.count);
+		view.renderCat();
+	}
 };
 
 var view = {
 
 	renderCatList: function() {
+
 		// get the cats so we can do stuff with them
 		var cats = octopus.getCats();
+		
 		//Make a cat identifier
 		var catListElem = document.getElementById('list-o-cats');
+		
 		for (var i = 0; i < cats.length; i++) {
 			var catIdentifier = cats[i];
+			
 			//Make a list of the cats names
 			var liElem = document.createElement('li');
 			liElem.textContent = catIdentifier.name;
 			catListElem.appendChild(liElem);
+			
+			//put an event listener on cat name.
+			liElem.addEventListener('click', (function(catNum){
+				return function () {
+					octopus.setCurrentCat(catNum);
+				};
+			})(i), false);
 		}
-		//adds an event listener for clicking on the cats name and create the cat
+	},
+	
+	//create a view for each cat
+	renderCat: function () {
+		var cat = octopus.getCurrentCat();
+		var catNameElem = document.getElementById('cat-name');
+		var catImgElem = document.getElementById('cat-img');
+		var catCountElem = document.getElementById('cat-count');
+		
+		catNameElem.textContent = cat.name;
+		catImgElem.src = cat.link;
+		catCountElem.textContent = cat.count;
+		
+		//Event listener on the cat's picture
+		catImgElem.addEventListener('click', octopus.plusOne, false);
 	}
-	//create a view for each cat with an event listener on the cat's picture, and a counter showing how many times the cat has been clicked
 };
 view.renderCatList();
+
+
+
+
+
+
+
+
